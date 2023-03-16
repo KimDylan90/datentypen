e12313d16a8c9b05bcca55c477c831afc5d27e6d
Kim Dylan <kim.wuebbena@docc.techstarter.de>
2023-03-16 13:45:59 +0100

    Typumwandlung added

---
+++ Typumwandlung.py
@@ -0,0 +1,14 @@
+# Beispiel 1: Umwandlung eines Integer in einen Float
+x = 1
+y = float(x)
+print(y)  # Ausgabe: 1.0
+
+# Beispiel 2: Umwandlung eines Strings in einen Integer
+x = "123"
+y = int(x)
+print(y)  # Ausgabe: 123
+
+# Beispiel 3: Umwandlung eines Floats in einen Integer
+x = 3.14
+y = int(x)
+print(y)  # Ausgabe: 3
